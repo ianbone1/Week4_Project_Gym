cadd0c6adca684fbe850f62dd03a68b258610a6e
ianbone1 <ianbone1@googlemail.com>
2019-02-02 17:13:19 +0000

    Managing class schedules now complete.

--- gym.rb
+++ gym.rb
@@ -2,6 +2,7 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('controllers/members.rb')
 require_relative('controllers/gymclasses.rb')
+require_relative('controllers/schedules.rb')
 
 get '/' do
   erb( :index )
