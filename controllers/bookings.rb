fd7fcf4c9cec9c20e6770ce00096ac1dcf7a541e
ianbone1 <ianbone1@googlemail.com>
2019-02-02 17:33:15 +0000

    Booking model cretaed.

---
+++ controllers/bookings.rb
@@ -0,0 +1,47 @@
+
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative( '../models/GymClass.rb' )
+also_reload( '../models/*' )
+
+#
+# # show schedules
+# get '/gymclasses/schedules/:id' do
+#   @gymclass = GymClass.find_by_id(params['id'])
+#   @schedules = @gymclass.schedule()
+#   erb ( :"gymclasses/schedule/index" )
+# end
+#
+# # edit schedule
+# get '/gymclasses/schedule/:id' do
+#   @schedule = Schedule.find_by_id(params['id'].to_i)
+#   @gymclass = GymClass.find_by_id(@schedule.gymclass_id)
+#   erb( :"gymclasses/schedule/edit" )
+# end
+# #delete schedules
+# post '/gymclasses/schedule/:id/delete' do
+#   gymclass = Schedule.find_by_id(params['id'])
+#   gymclass.delete()
+#   redirect to "/gymclasses"
+# end
+#
+# #do the update to the database for schedule changes
+# post '/gymclasses/schedule/:id' do
+#   updated_schedule = Schedule.new(params)
+#   updated_schedule.update
+#   refresh = "/gymclasses/schedules/" + params['gymclass_id'].to_s
+#   redirect to refresh
+# end
+#
+# post '/gymclasses/schedule' do
+#   new_schedule = Schedule.new(params)
+#   new_schedule.save
+#   refresh = "/gymclasses/schedules/" + params['gymclass_id'].to_s
+#   redirect to refresh
+# end
+#
+# get '/gymclasses/schedule/:id/create' do
+#   @gymclass = GymClass.find_by_id(params['id'])
+#   erb( :"gymclasses/schedule/create" )
+# end
---
+++ models/Booking.rb
@@ -0,0 +1,58 @@
+require_relative("./GymClass.rb")
+require_relative("./Member.rb")
+require_relative("./Schedule.rb")
+
+class Booking
+
+  # CREATE TABLE bookings(
+  #   id SERIAL8 PRIMARY KEY,
+  #   member_id INT8 REFERENCES members(id) ON DELETE CASCADE,
+  #   schedule_id INT8 REFERENCES schedules(id) ON DELETE CASCADE
+  # );
+  attr_reader :id, :member_id, :schedule_id
+  attr_writer :member_id, :schedule_id
+
+  def initialize(params)
+    @id = params['id'].to_i if params['id']
+    @member_id = params['member_id']
+    @schedule_id = params['schedule_id']
+  end
+
+  def self.map_data(data_hash)
+    result = data_hash.map { |data| self.new( data ) }
+    return result
+  end
+
+  def save()
+    sql = "INSERT INTO bookings (member_id, schedule_id)
+      VALUES ($1, $2) RETURNING id;"
+    values= [@member_id, @schedule_id]
+    @id = SQLRunner.execute(sql, values).first['id'].to_i
+  end
+
+  def update()
+    sql = "UPDATE bookings SET (member_id, schedule_id)
+      = ($1, $2) WHERE id = $3;"
+    values= [@member_id, @schedule_id, @id]
+    SQLRunner.execute(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM bookings WHERE id = $1;"
+    values = [@id]
+    SQLRunner.execute(sql, values)
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM bookings;"
+    SQLRunner.execute(sql)
+  end
+
+  def self.find_by_id(the_id)
+    sql = "SELECT * FROM bookings
+      WHERE id = $1;"
+    values = [the_id]
+    return self.map_data(SQLRunner.execute(sql, values)).first
+  end
+
+end
